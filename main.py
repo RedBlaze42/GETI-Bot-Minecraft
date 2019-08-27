b406572ae75acb4bf4eb5e6a12a9184c2892cb73
RedBlaze42 <allairehugo@gmail.com>
2019-08-28 00:33:48 +0200

    Empêcher les bot de trigger une commande

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
     content=message.content
-    user_permissions = message.channel.permissions_for(message.author)
-    if content.startswith("!mine ") and (len(content.split(" "))>=2):
-        await message.channel.trigger_typing()
-        cmd_return = await cmd_handler.handle_cmd(message)
-        if cmd_return!=None:
-            await message.channel.send(cmd_return)
+    if not message.author.bot:
+        user_permissions = message.channel.permissions_for(message.author)
+        if content.startswith("!mine ") and (len(content.split(" "))>=2):
+            await message.channel.trigger_typing()
+            cmd_return = await cmd_handler.handle_cmd(message)
+            if cmd_return!=None:
+                await message.channel.send(cmd_return)
         
 @client.event
 async def on_raw_reaction_add(payload):
